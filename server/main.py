c99b547f67298e9674b879a15c80d7108c91e8bf
Hao <leonghaozhi9@gmail.com>
2022-11-05 16:44:37 +0800

    added get/stocks endpoint

--- server/main.py
+++ server/main.py
@@ -1,21 +1,31 @@
-import json
 import yfinance as yf
 
 from typing import Optional, Union
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
 import pandas as pd
 
 app = FastAPI()
 
-def parse_df(df):
-    # print(df.columns)
-    # if isinstance(df.index, pd.MultiIndex):
+def nest(d: dict) -> dict:
+    result = {}
+    for key, value in d.items():
+        target = result
+        for k in key[:-1]:  # traverse all keys but the last
+            target = target.setdefault(k, {})
+        target[key[-1]] = value
+    return result
 
-    #if single index
-    res = df.to_json(orient="index")
-    parsed = json.loads(res)
-    return parsed
+def parse_df(df, ticker):
+    if not isinstance(df.columns, pd.MultiIndex):
+        df.columns = pd.MultiIndex.from_product([[ticker], df.columns])
+
+    df.reset_index(inplace=True)
+    df['Date'] = (df['Date'] - pd.Timestamp("1970-01-01"))
+    df.set_index('Date', inplace=True)
+    d = df.to_dict(orient='index')
+    res = {k: nest(v) for k, v in d.items()}
+    return res
 
 @app.get("/")
 def read_root():
@@ -29,7 +39,6 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def get_ticker_data(ticker: str):
     stock = yf.Ticker(ticker)
     return {"info": stock.info}
-
 """
     Reads a stock info for specified time period to current day
     - **ticker**: required ticker name, for multiple tickers, seperate with whitespace in string -> "AAPL MSFT AMZN"
@@ -37,19 +46,14 @@ def get_ticker_data(ticker: str):
     - **period**: an optional time period in string, ending with current date. Default is 2 years
         valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
 
-    - **interval**: an optional interval in string. Default is 1 day
-        valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
-
-    Response JSON format:
-
 """
 @app.get("/stocks")
-def get_ticker_data(ticker: str, period: Optional[str] = "2y", interval: Optional[str] = "1d"):
+def get_ticker_data(ticker: str, period: Optional[str] = "2y"):
     stocks = yf.download(
                 tickers = ticker,
                 period = period,
-                interval = interval
+                interval = "1d",    # interval is fixed to 1 day
+                group_by = "ticker"
             )
-    print(stocks)
-    return parse_df(stocks)
+    return parse_df(stocks, ticker)
 
