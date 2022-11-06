124f7ea0676b7d9fbfb81e74d861f00f6a6ef947
limguoquan <49220059+limguoquan@users.noreply.github.com>
2022-11-06 23:43:15 +0800

    Followed HZ code and added crypto request to frontend, along with default viz

--- client/src/components/Crypto.jsx
+++ client/src/components/Crypto.jsx
@@ -1,12 +1,32 @@
 import React, {useState} from 'react';
+import { useGetCryptoQuery } from '../services/cryptoApi';
+import Loader from './Loader';
+import { timeseriesmockdata } from './DataViz/mock_data/timeseriesmockdata';
 import SharedLayout from './SharedLayout';
 
 
 const Crypto = () => {
+  const [ symbol, setSymbol ] = useState('BTCUSDT');
+  const [ interval, setInterval ] = useState('1d');
+  const [ start_str, setStartStr ] = useState('2021.10.1');
+  const [ end_str, setEndStr ] = useState('2021.11.1');
+
+  const { data, isFetching } = useGetCryptoQuery({symbol: symbol, interval: interval, start_str: start_str, end_str: end_str});
+  console.log("data: ", data);
+
+  const getSymbol = (symbol) => {
+    setSymbol(symbol);
+  };
+
+  if (isFetching) return <Loader />;
+
   return (
     <>
       <div>Crypto</div>
-      <SharedLayout />
+      <SharedLayout 
+        data = {timeseriesmockdata}
+        getSymbol = {getSymbol}
+      />
     </>
   )
 }
