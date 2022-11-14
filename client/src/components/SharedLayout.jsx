9372048cd95256da4760f0a5175d0cc6fc04ab01
Hao <leonghaozhi9@gmail.com>
2022-11-14 22:08:01 +0800

    UI additions 2

--- client/src/components/SharedLayout.jsx
+++ client/src/components/SharedLayout.jsx
@@ -1,11 +1,17 @@
 import React, {useState} from 'react';
-import { Grid, CircularProgress } from '@mui/material';
+import { Grid, CircularProgress, Card, CardContent, Typography } from '@mui/material';
 import PriceChart from './DataViz/PriceChart';
 import MultiSelect from './MultiSelect';
 import StatsCard from './StatsCard'
+import { Content } from 'antd/lib/layout/layout';
 
 const SharedLayout = (props) => {
 	const [finishLoading, setFinishLoading] = useState(false);
+
+	var increase = false;
+	if (props.arima?.data?.originalPrice > props.arima?.data?.openingPrice) {
+		increase = true;
+	}
 
 	return (
 		<>
@@ -20,6 +26,61 @@ const SharedLayout = (props) => {
 						chooseTicker={props.getTicker}
 						choosePeriod={props.getPeriod}
 					/>
+					{
+						props.arima?.data?.openingPrice > 0 ? 
+							<Grid container spacing = {4} sx={{px: 1}}>
+								<Grid item xs = {3}>
+									<Card>
+										<CardContent sx={{display: 'flex', flexDirection: 'column', alignItems: 'flex-end', bgcolor: '#3462FF'}}>
+											<Typography variant="h5" sx={{color: '#edeeec', fontWeight: 500, mb: 0.5}}>
+												${props.arima?.data?.openingPrice}
+											</Typography>
+											<Typography sx={{ fontSize: 14, color: '#edeeec' }} gutterBottom>
+												OPEN
+											</Typography>
+										</CardContent>
+									</Card>
+								</Grid>
+								<Grid item xs = {3}>
+									<Card>
+										<CardContent sx={{display: 'flex', flexDirection: 'column', alignItems: 'flex-end', bgcolor: '#DCB400'}}>
+											<Typography variant="h5" sx={{color: '#edeeec', fontWeight: 500, mb: 0.5}}>
+												${props.arima?.data?.originalPrice}
+											</Typography>
+											<Typography sx={{ fontSize: 14, color: '#edeeec' }} gutterBottom>
+												CLOSE
+											</Typography>
+										</CardContent>
+									</Card>
+								</Grid>
+								<Grid item xs = {3}>
+									<Card>
+										<CardContent sx={{display: 'flex', flexDirection: 'column', alignItems: 'flex-end', bgcolor: increase ? '#00C100' : '#E10000'}}>
+											<Typography variant="h5" sx={{color: '#edeeec', fontWeight: 500, mb: 0.5}}>
+												{((props.arima?.data?.originalPrice - props.arima?.data?.openingPrice) / props.arima?.data?.openingPrice * 100).toFixed(3)}%
+											</Typography>
+											<Typography sx={{ fontSize: 14, color: '#edeeec' }} gutterBottom>
+												DAILY PERCENTAGE CHANGE
+											</Typography>
+										</CardContent>
+									</Card>
+								</Grid>
+								<Grid item xs = {3}>
+									<Card>
+										<CardContent sx={{display: 'flex', flexDirection: 'column', alignItems: 'flex-end', bgcolor: increase ? '#00C100' : '#E10000'}}>
+											<Typography variant="h5" sx={{color: '#edeeec', fontWeight: 500, mb: 0.5}}>
+												{((props.arima?.data?.originalPrice - props.arima?.data?.lastWeekPrice) / props.arima?.data?.lastWeekPrice * 100).toFixed(3)}%
+											</Typography>
+											<Typography sx={{ fontSize: 14, color: '#edeeec' }} gutterBottom>
+												WEEKLY PERCENTAGE CHANGE
+											</Typography>
+										</CardContent>
+									</Card>
+								</Grid>
+							</Grid> :
+						<></>
+					}
+					<br />
 					<Grid container spacing = {2}>
 
 						<Grid item xs = {6}>
@@ -46,6 +107,7 @@ const SharedLayout = (props) => {
 							<PriceChart
 								title = {"LSTM"}
 								predicted = {props.lstm?.data?.predictedTickerTimeSeries}
+								original = {props.arima?.data?.originalTickerTimeSeries}
 								isLoading = {props.lstm?.isLoading}
 							/>
 						</Grid>
@@ -55,32 +117,38 @@ const SharedLayout = (props) => {
 							<PriceChart
 								title = {"Random Forest"}
 								predicted = {props.rf?.data?.predictedTickerTimeSeries}
+								original = {props.rf?.data?.originalTickerTimeSeries}
 								isLoading = {props.rf?.isLoading}
 							/>
 						</Grid>
 
 					</Grid>
 					<br />
-					<Grid container spacing = {2}>
-						<Grid item xs = {4}>
-							<StatsCard 
-								model = {'ARIMA'}
-								query = {props.arima}
-							/>
-						</Grid>
-						<Grid item xs = {4}>
-							<StatsCard 
-								model = {'LSTM'}
-								query = {props.lstm}
-							/>
-						</Grid>
-						<Grid item xs = {4}>
-							<StatsCard 
-								model = {'Linear Regression'}
-								query = {props.rf}
-							/>
-						</Grid>
-					</Grid>
+
+					{
+						props.arima?.data?.openingPrice > 0 ? 
+							<Grid container spacing = {10}>
+								<Grid item xs = {4}>
+									<StatsCard 
+										model = {'ARIMA'}
+										query = {props.arima}
+									/>
+								</Grid>
+								<Grid item xs = {4}>
+									<StatsCard 
+										model = {'LSTM'}
+										query = {props.lstm}
+									/>
+								</Grid>
+								<Grid item xs = {4}>
+									<StatsCard 
+										model = {'Random Forest'}
+										query = {props.rf}
+									/>
+								</Grid>
+							</Grid> :
+							<></>
+					}
 				</div>
 			}
 		</>
