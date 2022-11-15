5a0603da26e7657045c886e30a77fe94fde1833f
limguoquan <49220059+limguoquan@users.noreply.github.com>
2022-11-15 10:38:34 +0800

    Change next day prediction to 7 days window for ARIMA

--- server/predictionModels/arima.py
+++ server/predictionModels/arima.py
@@ -56,13 +56,12 @@ def predictARIMA(df):
     # pyplot.plot(Predictions, color='red')
     # res=pyplot.show()
 
-    pred_val=StartARIMAForecasting(TestData[len(TestData)-10:],1,1,0)
+    pred_val=StartARIMAForecasting(TestData[len(TestData)-7:],7,1,0)
     pred_res.append([Timepoints[-1]+86400, pred_val])
 
     for i, item in enumerate(actual_res):
         new_time = item[0] * 1000
         actual_res[i][0] = new_time
-
     for i, item in enumerate(pred_res):
         new_time = item[0] * 1000
         pred_res[i][0] = new_time
